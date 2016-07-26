bd78812527661916677cefe621734ee9f0bfb617
Seizan Shimazaki <seizans@gmail.com>
2016-07-26 22:15:36 +0900

    Router, Handler を追加する

--- lib/coil.ex
+++ lib/coil.ex
@@ -1,2 +1,6 @@
 defmodule Coil do
+  def child_spec(port) do
+    # TODO(seizans): plug_opts と cowboy_opts を渡せる必要があれば渡せるようにする
+    Plug.Adapters.Cowboy.child_spec(:http, Coil.Router, [], port: port)
+  end
 end
---
+++ lib/coil/handler.ex
@@ -0,0 +1,17 @@
+defmodule Coil.Handler do
+  import Plug.Conn
+
+  def dispatch(conn, params) do
+    IO.inspect params
+    header_name = "x-coil"
+    case get_req_header(conn, header_name) do
+      [header_value] ->
+        IO.inspect header_value
+        conn
+        |> send_resp(200, "dispatch")
+      [] ->
+        conn
+        |> send_resp(400, "dispatch")
+    end
+  end
+end
