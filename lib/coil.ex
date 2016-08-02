04e4c5c7168104ca0d53e47fd026ccf8f95a21c3
Seizan Shimazaki <seizans@gmail.com>
2016-08-02 11:43:48 +0900

    start は cowboy_opts を受け取るようにする

--- lib/coil.ex
+++ lib/coil.ex
@@ -1,14 +1,12 @@
 defmodule Coil do
-  # TODO(seizans): service 複数に対応できるようにするか
-  @spec start(atom(), binary(), binary(), map(), non_neg_integer()) :: {:ok, pid()}
-  def start(app_name, service_name, coil_header_name, dispatch_conf, port) do
-    # TODO(seizans): ets に格納した方がいいかどうか
+  @spec start(atom, binary, binary, map, keyword) :: {:ok, pid}
+  def start(app_name, service_name, coil_header_name, dispatch_conf, cowboy_opts \\ []) do
+    # TODO(seizans): opts は ets に格納した方がいいのかどうか
     # TODO(seizans): dispatch_conf 内の operation に対応する mod:fun が存在するか起動時にチェックする
     opts = %{service_name: service_name,
              coil_header_name: coil_header_name,
              dispatch_conf: dispatch_conf}
     Coil.JsonSchema.start(app_name, service_name, dispatch_conf)
-    # TODO(seizans): cowboy_opts を渡せる必要があるか検討する
-    Plug.Adapters.Cowboy.http(Coil.Handler, opts, [port: port])
+    Plug.Adapters.Cowboy.http(Coil.Handler, opts, cowboy_opts)
   end
 end
